82a21539a01eb706691408301c7a3b7a92f25544
Oleg M <seeds.bite@gmail.com>
2016-11-22 03:47:34 +0300

    primary functionality ajaxgrid and primary markup admin.html.twig

--- web/ajaxgrid.js
+++ web/ajaxgrid.js
@@ -1,7 +1,51 @@
-/**
- * Created by sumlbel on 17.11.16.
- */
-jQuery.fn.ajaxgrid = function(o){
+(function ($) {
+    $.fn.ajaxgrid = function (options) {
+        var root = this;
+        var table, tbody;
+        var headerRow;
+        var filters, pagination;
+        var request = {
+            rows: options.rowsPerPage
+        };
+
+        completeOptions(options);
+        createBaseElements();
+        sendInitRequest();
+
+        return root;
+
+        function completeOptions(options) {
+            if (!options.dataUrl){
+                throw 'check your dataUrl'
+            }
+            if (!options.sortableColumns){
+                options.sortableColumns = [];
+            }
+            if (!options.filterableColumns){
+                options.filterableColumns = [];
+            }
+            if (!options.rowsPerPage){
+                options.rowsPerPage = 10;
+            }
+        }
+
+        function createBaseElements() {
+            table = $('table').appendTo(root);
+            tbody = $('tbody').appendTo(table);
+            headerRow = $('tr').appendTo(table);
+            filters = $('div').appendTo(root).addClass('filters');
+            pagination = $('div').appendTo(root).addClass('pagination');
+        }
+
+        function sendInitRequest() {
+            $.fn.getJSON(options.dataUrl, request, function (json) {
+                var columns = Object.getOwnPropertyNames(json.data[0]);
+            });
+
+        }
 
 
-}
+
+    };
+
+})(jQuery)
